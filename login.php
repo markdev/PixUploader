9a51929fed95f1b2b579d6228f9cb8d0bacb443d
Mark Karavan <mark.karavan@gmail.com>
2015-02-21 00:45:45 -0500

    right, you have to label your inputs with 'name', I forgot that

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 	if (isset($_SESSION['user']) && $_SESSION['user']['permission'] == "admin") {
 		// admin dashboard
 		header("Location: http://localhost/dashboard_admin.php");
@@ -7,23 +7,26 @@
 		// user dashboard
 		header("Location: http://localhost/dashboard_user.php");
 	}
+*/
+	print_r($_POST);
 
 	$page = '
+	<!DOCTYPE HTML>
 	<html>
 		<head>
 
 		</head>
 		<body>
 			<h1>Login</h1>
-			<form action="login.php" method="post">
+			<form action="login.php" name="login" method="post">
 				<table>
 					<tr>
 						<td>Email: </td>
-						<td><input type="text" id="email"/></td>
+						<td><input type="text" name="email"/></td>
 					</tr>
 					<tr>
 						<td>Password: </td>
-						<td><input type="password" id="password"/></td>
+						<td><input type="password" name="password"/></td>
 					</tr>
 					<tr>
 						<td colspan="2"><input type="submit"/></td>
