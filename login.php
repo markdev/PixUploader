87f560a663d52bfe57ad13b68dd6d9e4d3b0f349
Mark Karavan <mark.karavan@gmail.com>
2015-02-21 00:21:46 -0500

    adds login html

--- login.php
+++ login.php
@@ -1,5 +1,39 @@
 <?php
 
-	echo "<h1>Login Page</h1>";
+	if (isset($_SESSION['user']) && $_SESSION['user']['permission'] == "admin") {
+		// admin dashboard
+		header("Location: http://localhost/dashboard_admin.php");
+	} else if (isset($_SESSION['user']) && $_SESSION['user']['permission'] == "user") {
+		// user dashboard
+		header("Location: http://localhost/dashboard_user.php");
+	}
 
+	$page = '
+	<html>
+		<head>
+
+		</head>
+		<body>
+			<h1>Login</h1>
+			<form action="login.php" method="post">
+				<table>
+					<tr>
+						<td>Email: </td>
+						<td><input type="text" id="email"/></td>
+					</tr>
+					<tr>
+						<td>Password: </td>
+						<td><input type="password" id="password"/></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit"/></td>
+					</tr>
+				</table>
+			</form>
+			<a href="createUser.php">Create an account</a>
+		</body>
+	</html>
+	';
+
+	echo $page;
 ?>
