99fd75e642510bb96f7a8dab74727eadb4b506c7
Mark Karavan <mark.karavan@gmail.com>
2015-02-21 17:33:58 -0500

    hashes the file for storage purposes

--- dashboard_userAjax.php
+++ dashboard_userAjax.php
@@ -4,6 +4,14 @@
 	print_r($_POST);
 	print_r($_FILES);
 
+	//let's make a hash for the name
+	$file_name = basename($_FILES["fileToUpload"]["name"]);
+	$extension_array = explode(".", $file_name);
+	$extension = array_pop($extension_array);
+	$file_hash = bin2hex(mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)) . "." . $extension;
+
+
+/*
 	$target_file = "images/" . basename($_FILES["fileToUpload"]["name"]);
 
 	// Check if image file is a actual image or fake image
@@ -18,5 +26,6 @@
 	} else {
 		echo "Not submitted";
 	}
+*/
 ?>
 
