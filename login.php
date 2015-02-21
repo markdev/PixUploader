256a14344cab9e4f55054086026897940027e9ac
Mark Karavan <mark.karavan@gmail.com>
2015-02-21 00:55:14 -0500

    testing post

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 	if (isset($_SESSION['user']) && $_SESSION['user']['permission'] == "admin") {
 		// admin dashboard
 		header("Location: http://localhost/dashboard_admin.php");
@@ -7,8 +7,23 @@
 		// user dashboard
 		header("Location: http://localhost/dashboard_user.php");
 	}
-*/
-	print_r($_POST);
+
+	// Let's initialize the database
+	define('DATABASE', "localhost");
+	define('DATABASE_USER', "root");
+	define('DATABASE_PASS', "foo"); // this is my local mysql password.  Now you know all my secrets.
+	define('DATABASE_NAME', "PixUploader");
+	$db = mysql_connect(DATABASE, DATABASE_USER, DATABASE_PASS, DATABASE_NAME)
+		or die("Unable to connect to MySQL");
+
+	mysql_select_db(DATABASE_NAME) 
+		or die("Unable to select database");
+
+	if (!empty($_POST)) {
+		print_r($_POST);
+	} else {
+		echo "NO POST";
+	}
 
 	$page = '
 	<!DOCTYPE HTML>
@@ -39,4 +54,6 @@
 	';
 
 	echo $page;
+
+	mysql_close($db);
 ?>
