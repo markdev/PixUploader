8b72d3bf3401dd5182401dc6a3af2e993fdf48ce
Mark Karavan <mark.karavan@gmail.com>
2015-02-20 23:17:19 -0500

    talks to database

--- index.php
+++ index.php
@@ -3,11 +3,23 @@
 	echo "<h1>Hello uploader</h1>";
 
 	// Let's initialize the database
-	define('DATABASE_NAME', "localhost");
+	define('DATABASE', "localhost");
 	define('DATABASE_USER', "root");
 	define('DATABASE_PASS', "foo"); // this is my local mysql password.  Now you know all my secrets.
-	$db = mysql_connect(DATABASE_NAME, DATABASE_USER, DATABASE_PASS)
+	define('DATABASE_NAME', "PixUploader");
+	$db = mysql_connect(DATABASE, DATABASE_USER, DATABASE_PASS, DATABASE_NAME)
 		or die("Unable to connect to MySQL");
+
+	mysql_select_db(DATABASE_NAME) 
+		or die("Unable to select database");
+
+	$sql = "SELECT * from Users";
+	$result = mysql_query($sql);
+	while ($row = mysql_fetch_array($result)) {
+		print_r($row);
+	}
+
+	mysql_close($db);
 
 	echo "Connected to Mysql";
 ?>
