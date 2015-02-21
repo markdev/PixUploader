12e50a3cfe3975752baed5b363bf5aaa44a291b8
Mark Karavan <mark.karavan@gmail.com>
2015-02-20 23:06:40 -0500

    connects to mysql

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 
 	echo "<h1>Hello uploader</h1>";
 
+	// Let's initialize the database
+	define('DATABASE_NAME', "localhost");
+	define('DATABASE_USER', "root");
+	define('DATABASE_PASS', "foo"); // this is my local mysql password.  Now you know all my secrets.
+	$db = mysql_connect(DATABASE_NAME, DATABASE_USER, DATABASE_PASS)
+		or die("Unable to connect to MySQL");
+
+	echo "Connected to Mysql";
 ?>
