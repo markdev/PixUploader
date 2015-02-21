52d2e579e8b0865ddb8beb65ad6e2c7738564e68
Mark Karavan <mark.karavan@gmail.com>
2015-02-20 23:57:31 -0500

    redirects to pages on start

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-	echo "<h1>Hello uploader</h1>";
-
 	// Let's initialize the database
 	define('DATABASE', "localhost");
 	define('DATABASE_USER', "root");
@@ -13,11 +11,30 @@
 	mysql_select_db(DATABASE_NAME) 
 		or die("Unable to select database");
 
+	/*
 	$sql = "SELECT * from Users";
 	$result = mysql_query($sql);
 	while ($row = mysql_fetch_array($result)) {
 		print_r($row);
 	}
+	*/
+
+	// testing
+	//$_SESSION['admin'] = array('permission' => 'admin');
+	//$_SESSION['user'] = array('permission' => 'user');
+
+	if (empty($_SESSION['user'])) {
+		// forces redirect to login page if no one is logged in
+		header("Location: http://localhost/login.php");
+	} else if (isset($_SESSION['user']) && $_SESSION['user']['permission'] == "admin") {
+		// admin dashboard
+		header("Location: http://localhost/dashboard_admin.php");
+	} else if (isset($_SESSION['user']) && $_SESSION['user']['permission'] == "user") {
+		// user dashboard
+		header("Location: http://localhost/dashboard_user.php");
+	}
+
+	print_r($_SESSION);
 
 	mysql_close($db);
 
