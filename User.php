d382c24ca6a738709ffc3028e9395e11e1b783a6
Mark Karavan <mark.karavan@gmail.com>
2015-02-21 19:34:20 -0500

    moves user creation to User class

--- User.php
+++ User.php
@@ -2,7 +2,24 @@
 
 class User {
 
-	public function __construct('email', 'permission') {
+	public function __construct($email, $permission) {
 
 	}
+
+	public function login() {
+
+	}
+
+	public static function create($email, $password, $permission) {
+		$sql = 'INSERT INTO Users (email, password, permission, created_on) VALUES ("' . $email .'", md5("' . $password . '"), "' . $permission . '", NOW())';
+		$res = mysql_query($sql);
+		if ($res == 1) {
+			$_SESSION['user'] = array('email' => $email, 'permission' => $permission);
+			return true;
+		} else {
+			return false;
+		}		
+	}
 }
+
+?>
--- createUserAjax.php
+++ createUserAjax.php
@@ -1,18 +1,8 @@
 <?php
 	require_once("dbheader.php");
-	
-	print_r($_POST);
+	require_once("User.php");
 
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$permission = $_POST['permission'];
-	$sql = 'INSERT INTO Users (email, password, permission, created_on) VALUES ("' . $email .'", md5("' . $password . '"), "' . $permission . '", NOW())';
-	$res = mysql_query($sql);
-	if ($res == 1) {
-		$_SESSION['user'] = array('email' => $email, 'permission' => $permission);
-		echo true;
-	}
+	echo User::create($_POST['email'], $_POST['password'], $_POST['permission']);
 
 	mysql_close($db);
-
 ?>
